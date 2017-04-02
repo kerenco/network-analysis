c268a83a1d730591d0452902c195c4635db27b4f
roi3579 <roi3579@gmail.com>
2017-04-02 18:05:51 +0300

    fix SGD

--- learning/machineLearning/LearningPhase.py
+++ learning/machineLearning/LearningPhase.py
@@ -30,7 +30,7 @@ class learningPhase:
         if algo == 'RBF-SVM':
             clf = SVC(class_weight="balanced", C=0.01)
         if algo == 'SGD':
-            cld = SGDClassifier(alpha=0.0001, average=False, class_weight=None, epsilon=0.1,eta0=0.0,
+            clf = SGDClassifier(alpha=0.0001, average=False, class_weight=None, epsilon=0.1,eta0=0.0,
                                 fit_intercept=True, l1_ratio=0.15,learning_rate='optimal', loss='hinge',
                                 n_iter=5, n_jobs=1,penalty='l2', power_t=0.5, random_state=None, shuffle=True,
                                 verbose=0, warm_start=False)
