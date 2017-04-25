c991ffec77116c4245d6e5c6d15c0783cdfc1dc6
roi3579 <roi3579@gmail.com>
2017-04-25 18:09:04 +0300

    adding epinions,slashdot
    *mains (only features calculation)
    * general + motif3

---
+++ graphs_mains/slashdot_main.py
@@ -0,0 +1,55 @@
+import os
+import sys
+sys.path.append( os.getcwd() +"//..")
+import featuresList
+from features_calculator import featuresCalculator
+from learning.TagsLoader import TagsLoader
+import main_manager as mm
+
+currentDirectory = str(os.getcwd())
+
+if __name__ == "__main__":
+
+    #step 1: features calculate
+    wdir = os.getcwd()
+    file_in = str(wdir) + r'/../data/directed/social_sign/slashdot/input/slashdot.txt'
+
+    output_dir = str(wdir) + r'/../data/directed/social_sign/slashdot/features'
+
+    calculator = featuresCalculator()
+    features_list = featuresList.featuresList(True, 'edges').getFeatures()
+    features_list.remove('motif4')
+    features_list.remove('flow')
+    print features_list
+    result = calculator.calculateFeatures(features_list, file_in, output_dir, True, 'edges')
+    print result[0]
+
+    #step 2: learning phase
+    # classification_wiki_result = ['wiki-tags']
+    # ml_algos = ['adaBoost', 'RF', 'L-SVM', 'RBF-SVM']
+    # directory_tags_path = str(wdir) + r'/../data/directed/wiki-rfa/tags/'
+    # result_path = str(wdir) + r'/../data/directed/wiki-rfa/results/'
+    # tagsLoader = TagsLoader(directory_tags_path, classification_wiki_result)
+    # tagsLoader.Load()
+    #
+    #
+    # gnx = result[0]
+    # map_fetures = result[1]
+    # number_of_learning_for_mean = 1.0
+    #
+    # deep = False
+    # if(deep):
+    #     mm.deepLearning(gnx,map_fetures,number_of_learning_for_mean=3.0,classifications=classification_wiki_result,tags_loader=tagsLoader,result_path=result_path)
+    # else:
+    #     mm.machineLearning(gnx,map_fetures,number_of_learning_for_mean=number_of_learning_for_mean,
+    #                        classifications=classification_wiki_result,
+    #                        ml_algos=ml_algos,
+    #                        tags_loader=tagsLoader,
+    #                        result_path=result_path)
+
+
+    #step 3: vizualization
+
+
+
+
