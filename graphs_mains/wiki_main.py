4d671239c45a9bb71bfbd75b4e95a2180cb15147
roi3579 <roi3579@gmail.com>
2017-04-27 11:47:53 +0300

    wiki main for compute only features on edges

--- graphs_mains/wiki_main.py
+++ graphs_mains/wiki_main.py
@@ -12,39 +12,39 @@ if __name__ == "__main__":
 
     #step 1: features calculate
     wdir = os.getcwd()
-    file_in = str(wdir) + r'/../data/directed/wiki-rfa/input/wiki.txt'
+    file_in = str(wdir) + r'/../data/directed/social_sign/wiki/input/wiki.txt'
 
-    output_dir = str(wdir) + r'/../data/directed/wiki-rfa/features'
+    output_dir = str(wdir) + r'/../data/directed/social_sign/wiki/features'
 
     calculator = featuresCalculator()
-    features_list = featuresList.featuresList(True, 'nodes').getFeatures()
+    features_list = featuresList.featuresList(True, 'edges').getFeatures()
     features_list.remove('motif4')
     features_list.remove('flow')
-    result = calculator.calculateFeatures(features_list, file_in, output_dir, True, 'nodes')
+    result = calculator.calculateFeatures(features_list, file_in, output_dir, True, 'edges')
 
 
     #step 2: learning phase
-    classification_wiki_result = ['wiki-tags']
-    ml_algos = ['adaBoost', 'RF', 'L-SVM', 'RBF-SVM']
-    directory_tags_path = str(wdir) + r'/../data/directed/wiki-rfa/tags/'
-    result_path = str(wdir) + r'/../data/directed/wiki-rfa/results/'
-    tagsLoader = TagsLoader(directory_tags_path, classification_wiki_result)
-    tagsLoader.Load()
-
-
-    gnx = result[0]
-    map_fetures = result[1]
-    number_of_learning_for_mean = 10.0
-
-    deep = False
-    if(deep):
-        mm.deepLearning(gnx,map_fetures,number_of_learning_for_mean=3.0,classifications=classification_wiki_result,tags_loader=tagsLoader,result_path=result_path)
-    else:
-        mm.machineLearning(gnx,map_fetures,number_of_learning_for_mean=10.0,
-                           classifications=classification_wiki_result,
-                           ml_algos=ml_algos,
-                           tags_loader=tagsLoader,
-                           result_path=result_path)
+    # classification_wiki_result = ['wiki-tags']
+    # ml_algos = ['adaBoost', 'RF', 'L-SVM', 'RBF-SVM']
+    # directory_tags_path = str(wdir) + r'/../data/directed/wiki-rfa/tags/'
+    # result_path = str(wdir) + r'/../data/directed/wiki-rfa/results/'
+    # tagsLoader = TagsLoader(directory_tags_path, classification_wiki_result)
+    # tagsLoader.Load()
+    #
+    #
+    # gnx = result[0]
+    # map_fetures = result[1]
+    # number_of_learning_for_mean = 10.0
+    #
+    # deep = False
+    # if(deep):
+    #     mm.deepLearning(gnx,map_fetures,number_of_learning_for_mean=3.0,classifications=classification_wiki_result,tags_loader=tagsLoader,result_path=result_path)
+    # else:
+    #     mm.machineLearning(gnx,map_fetures,number_of_learning_for_mean=10.0,
+    #                        classifications=classification_wiki_result,
+    #                        ml_algos=ml_algos,
+    #                        tags_loader=tagsLoader,
+    #                        result_path=result_path)
 
 
     #step 3: vizualization
